db0a393133e6691bf058d1c21bff5756900c221a
Elida Lapa cerros <elidalapa@gmail.com>
2021-02-23 20:39:45 -0600

    parcial

---
+++ pregunta3/pregunta3/script.js
@@ -0,0 +1,17 @@
+function sendData(){
+    var name = document.getElementById("name").value
+    var orientation = document.getElementById("orientation").value 
+    var quantity = document.getElementById("quantity").value 
+
+    if(quantity>50){
+        var status = ("+ " +50);
+    }else if(quantity<50){
+        var status = ("- " +50);
+    }else if (quantity=50){
+        var status = ("= " +50);
+    }
+
+    document.getElementById("Data").insertRow(-1).innerHTML = '<tr><td>'+name+'<tr><td>'+orientation+'<tr><td>'+quantity+'<tr><td>'+status+'<tr><td>'
+}
+
+function getGanador()
